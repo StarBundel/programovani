2f9cb935aebcb696aea013c5492fefaec34f698c
StarBundel <120466908+StarBundel@users.noreply.github.com>
2022-12-13 12:58:20 +0100

    Test
    moje soubory z programka

---
+++ Test_volat.cpp
@@ -0,0 +1,28 @@
+#define _WIN32_WINNT 0x601
+#include <iostream>
+#include <time.h>
+#include <Windows.h>
+#include <cstdlib>
+#include <ctime>
+#include <stdlib.h>
+#include <unistd.h>
+#include <conio.h>
+#include <conio.h>
+#include <fstream>
+#include <string>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <stdlib.h>
+#
+//#include <Pouzit.cpp>
+using namespace std;
+
+
+
+int main(){
+
+void pocet();
+
+
+return 0;
+}
